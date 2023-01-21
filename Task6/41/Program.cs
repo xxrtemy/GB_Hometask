192671222398ac9788cd9c2e6b7c04c7fbf82321
xxrtemy <rebelgamechannel@gmail.com>
2023-01-21 16:36:10 +0300

    Task6

---
+++ Task6/41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите кол-во вводимых чисел: ");
+int _countPositiveNumbers = 0;
+int _count = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[_count];
+Console.Write("Введите число: ");
+for (int i = 0; i < arr.Length; i++)
+   arr[i] = Convert.ToInt32(Console.ReadLine()); 
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) 
+        _countPositiveNumbers++;
+}
+Console.Write($"Кол-во положительных чисел: {_countPositiveNumbers}");
---
+++ Task6/43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+if ((k1==k2)&&(b1==b2))
+    Console.Write("Одна из прямых лежит на другой");
+else if (k1==k2)
+    Console.Write("Прямые параллельны и не пересекаются");
+    else
+        {
+        double x = -(b1 - b2) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Пересечение в точке: ({x};{y})");
+        }
